4c5e27ab091201daf78d2867925302137a57c9a0
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-09-22 22:12:38 +0000

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,19 @@
+class Song < ApplicationRecord
+  validates :title, presence: true
+  validates :released, exclusion: { in: [nil] }
+  validates :release_year, presence: true, if: :is_released?, numericality: { only_integer: true, less_than_or_equal_to: 2019 }
+  validates :artist_name, presence: true
+  validate :title_unique_this_year?
+
+  def title_unique_this_year?
+    match = Song.find_by(release_year: release_year, artist_name: artist_name, title: title)
+
+    if match != nil
+      errors.add(:title, "can't be released twice in the same year")
+    end
+  end
+
+  def is_released?
+    released
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :songs, only: [:index, :new, :show, :edit, :create]
+  patch '/songs/:id', to: 'songs#update'
+  delete '/songs/:id', to: 'songs#destroy'
 end
